686e5dab06f2c59f681da79b4511c15cce809219
bloggustos-stack <bloggustos@gmail.com>
2026-03-06 20:53:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -929,13 +929,13 @@ def admin():
 def download_pdf(filename):
     return send_file(filename, as_attachment=True)
 
-@app.route('/icon-192.png')
+    @app.route('/icon-192.png')
 def serve_icon_192():
-    return app.send_static_file('icon-192.png')
+    return send_file('icon-192.png')
 
 @app.route('/icon-512.png')
 def serve_icon_512():
-    return app.send_static_file('icon-512.png')
+    return send_file('icon-512.png')
 
 @app.route('/service-worker.js')
 def service_worker():
