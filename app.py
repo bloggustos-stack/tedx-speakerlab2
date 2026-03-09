3f19b1be5803ca3001a4f2c716d473d19a397d1f
bloggustos-stack <bloggustos@gmail.com>
2026-03-09 13:57:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -932,8 +932,7 @@ def download_pdf(filename):
 @app.route('/download-guide')
 @login_required
 def download_guide():
-  import os
-    return send_file(os.path.join(app.root_path, 'static', 'Ghid_Arhetipuri_TEDx.pdf'), as_attachment=True)
+    return send_file('templates/static/Ghid_Arhetipuri_TEDx.pdf', as_attachment=True)
 
 @app.route('/icon-192.png')
 def serve_icon_192():
